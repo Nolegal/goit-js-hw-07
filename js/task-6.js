9617f64fcccc2ddaa0aa45650245ff3d2729d95b
Nolegal <nolegal@ukr.net>
2024-12-02 17:33:36 +0200

    6th commit

--- js/task-6.js
+++ js/task-6.js
@@ -1,6 +1,6 @@
 const createBox=document.querySelector('#controls button[data-create]');
 const destroyBox=document.querySelector('#controls button[data-destroy]');
-const amount=document.querySelector('#controls input').value;
+const amount=document.querySelector('#controls input');
 const boxes=document.querySelector('#boxes');
 
 function getRandomHexColor() {
@@ -8,22 +8,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-let step=30
+
 const createBoxes=(amount)=>{
+  let step=30;
+  const elements=[];
   for (let i = 0; i < amount; i++) {
     let div = document.createElement("div");
-    div.style.color = getRandomHexColor();
-    step+=10
+    div.style.backgroundColor = getRandomHexColor();
+   
     div.style.height = `${step}px`;
     div.style.width = `${step}px`;
-    boxes.appendChild(div);
-    
+    step += 10;
+    elements.push(div);
+   
   }
-  
+  boxes.append(...elements) 
+  boxes.reset 
+ 
 }
 
 
-createBox.addEventListener("click", createBoxes)
+createBox.addEventListener("click", (event)=>{
+  event.preventDefault();
+  const form=event.target;
+  const inputValue=Number(form.amount.value.trim());
+  createBoxes(inputValue);
+  form.reset();
+}
+  )
 
 destroyBox.addEventListener("click",()=>{
   boxes.innerHTML=''
